586d5661357e69a6accfa707ca3248f791457e09
Cui-y <cuiyang673308817@gmail.com>
2018-12-22 18:10:40 +0800

    complete related productions & actionsheet & dialog

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+(function($) {
+  $('.header img').click((e) => {
+    $('.action-sheet-modal').show()
+    $('.action-sheet').addClass('up')
+    e.stopPropagation()
+  })
+
+  $('.action-sheet').click((e) => {
+    e.stopPropagation()
+  })
+
+  $(document).click(() => {
+    $('.action-sheet-modal').hide()
+    $('.action-sheet').removeClass('up')
+  })
+})(Zepto)
